c8a13432324378a4f1b4d8c97420d779f9c63df3
Pingvu <nhlpool27@gmail.com>
2023-03-17 14:00:44 +0100

    Add project files.

---
+++ OV1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace Ålder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv din ålder:");
+            int Dinålder = int.Parse(Console.ReadLine());
+
+            Kollaålder (Dinålder);
+        }
+
+        static void Kollaålder (int ålder)
+        {
+            if (ålder >= 18)
+            {
+                Console.WriteLine("Du är myndig.");
+            }
+            else
+            {
+                Console.WriteLine("Du är inte myndig.");
+            }
+        }
+    }
+}
